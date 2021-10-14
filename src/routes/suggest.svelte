8dea70ac01d636bf5a32d17c758b9133f723dbf8
elibates <eli.m.bates@gmail.com>
2021-10-14 21:16:42 +0200

    feedback form component

--- src/routes/suggest.svelte
+++ src/routes/suggest.svelte
@@ -1,8 +1,10 @@
 <script>
   import SuggestionBox from '../components/Form/SuggestionBox.svelte';
+  import FeedbackForm from '../components/Form/FeedbackForm.svelte';
 </script>
 
 <SuggestionBox />
+<FeedbackForm />
 
 <style>
 </style>
